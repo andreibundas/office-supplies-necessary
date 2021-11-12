ce314cd3eb0f379fee4edfab7cc27dde0b7790c3
Andrei Bundas <andrei.bundas@gmail.com>
2021-11-12 16:22:25 +0200

    configured database connection

---
+++ src/main/java/org/fasttrackit/domain/Supplies.java
@@ -0,0 +1,83 @@
+package org.fasttrackit.domain;
+
+import java.time.LocalDate;
+
+public class Supplies {
+
+    private double id;
+    private String department;
+    private String supplies;
+    private double quantityPcsPckgs;
+    private double valueRON;
+    private LocalDate deliveryDate;
+    private boolean completed;
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSupplies() {
+        return supplies;
+    }
+
+    public void setSupplies(String supplies) {
+        this.supplies = supplies;
+    }
+
+    public double getQuantityPcsPckgs() {
+        return quantityPcsPckgs;
+    }
+
+    public void setQuantityPcsPckgs(double quantityPcsPckgs) {
+        this.quantityPcsPckgs = quantityPcsPckgs;
+    }
+
+    public double getValueRON() {
+        return valueRON;
+    }
+
+    public void setValueRON(double valueRON) {
+        this.valueRON = valueRON;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplies{" +
+                "id=" + id +
+                ", department='" + department + '\'' +
+                ", supplies='" + supplies + '\'' +
+                ", quantityPcsPckgs=" + quantityPcsPckgs +
+                ", valueRON=" + valueRON +
+                ", deliveryDate=" + deliveryDate +
+                ", completed=" + completed +
+                '}';
+    }
+}
